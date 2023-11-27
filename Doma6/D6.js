794db309c00fd49b00e525cca72053006db7e428
dejangruevski <dejangruevski@gmail.com>
2023-11-27 17:25:11 +0100

    added homework 6

---
+++ Doma6/D6.js
@@ -0,0 +1,12 @@
+// - Формулар за регистрација на корисник (име, презиме, email, password..). Испечатете ги вредностите кои што се внесени во формуларот во конзола
+
+function onClickFunction() {
+    let firstname = document.getElementById("first_name").value
+    console.log(firstname)
+    let lastname = document.getElementById("last_name").value
+    console.log(lastname)
+    let email = document.getElementById("email").value
+    console.log(email)
+    let pass = document.getElementById("pass").value
+    console.log(pass)
+}
